d736133b5601fda8c6b233070bc4f2f1c71e8578
Star <t108590061@ntut.org.tw>
2021-12-28 01:27:48 +0800

    updates

--- app/src/main/scala/predictorOfFlightDelay/predictor.scala
+++ app/src/main/scala/predictorOfFlightDelay/predictor.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler, OneHotEncoder}
 import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
 import org.apache.spark.ml.evaluation.{RegressionEvaluator}
-import org.apache.spark.ml.regression.{LinearRegression, RandomForestRegressor}//GBTRegressor
+import org.apache.spark.ml.regression.{LinearRegression}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.mllib.evaluation.RegressionMetrics
 import org.apache.spark.sql.functions.col
@@ -17,20 +17,9 @@ object Flight {
 
   def main(args: Array[String]) {
     print("\n")
-    print("Where is your dataset located? (provide full path on disk) \n")
+    print("Dataset Location? (provide full path on disk) \n")
 
     val dataPath = readLine()
-
-    var mlTechnique: Int = 0
-
-    while(mlTechnique != 1 && mlTechnique != 2){ //&&mlTechnique != 3
-      print("\n")
-      print("Which machine learning technique do you want to use? \n")
-      print("[1] Linear Regression \n")
-      print("[2] Random Forest Trees \n")
-      //print("[3] Gradient-Boosted Trees \n")
-      mlTechnique = readInt()
-    }
 
     print("\n")
     print("Use categorical features? (yes/no) \n")
@@ -111,106 +100,42 @@ object Flight {
     
     
 
-    mlTechnique match {
-      case 1 => 
-        val lr = new LinearRegression()
+    val lr = new LinearRegression()
           .setLabelCol("DelayOutputVar")
           .setFeaturesCol("features")
-        val paramGrid = new ParamGridBuilder()
+    val paramGrid = new ParamGridBuilder()
           .addGrid(lr.regParam, Array(0.1, 0.01))
           .addGrid(lr.fitIntercept)
           .addGrid(lr.elasticNetParam, Array(0.0, 1.0))
           .build()
 
-        val steps:Array[org.apache.spark.ml.PipelineStage] = if(useCategorical){
+    val steps:Array[org.apache.spark.ml.PipelineStage] = if(useCategorical){
                                                                 categoricalIndexers ++ categoricalEncoders ++ Array(assembler, lr)
                                                               }else{
                                                                 Array(assembler, lr)
                                                               }
 
-        val pipeline = new Pipeline().setStages(steps)
+    val pipeline = new Pipeline().setStages(steps)
 
-        val tvs = new TrainValidationSplit()
+    val tvs = new TrainValidationSplit()
           .setEstimator(pipeline)
           .setEvaluator(new RegressionEvaluator().setLabelCol("DelayOutputVar"))
           .setEstimatorParamMaps(paramGrid)
           .setTrainRatio(0.7)
 
-        val Array(training, test) = data.randomSplit(Array(0.70, 0.30), seed = 12345)
+    val Array(training, test) = data.randomSplit(Array(0.70, 0.30), seed = 12345)
 
-        val model = tvs.fit(training)
+    val model = tvs.fit(training)
 
-        val holdout = model.transform(test).select("prediction", "DelayOutputVar")
+    val holdout = model.transform(test).select("prediction", "DelayOutputVar")
 
-        val rm = new RegressionMetrics(holdout.rdd.map(x =>
-          (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
+    val rm = new RegressionMetrics(holdout.rdd.map(x => (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
 
-        println("sqrt(MSE): " + Math.sqrt(rm.meanSquaredError))
-        println("mean absolute error: " + 	rm.meanAbsoluteError)
-        println("R Squared: " + rm.r2)                         
-        println("Explained Variance: " + rm.explainedVariance + "\n")
+    println("sqrt(MSE): " + Math.sqrt(rm.meanSquaredError))
+    println("mean absolute error: " + 	rm.meanAbsoluteError)
+    println("R Squared: " + rm.r2)
+    println("Explained Variance: " + rm.explainedVariance + "\n")
 
-      case 2 =>
-        val rf = new RandomForestRegressor()
-          .setNumTrees(10)
-          .setMaxDepth(10)
-          .setLabelCol("DelayOutputVar")
-          .setFeaturesCol("features")
-
-
-        val steps:Array[org.apache.spark.ml.PipelineStage] = if(useCategorical){
-                                                                categoricalIndexers ++ categoricalEncoders ++ Array(assembler, rf)
-                                                              }else{
-                                                                Array(assembler, rf)
-                                                              }
-
-        val pipeline = new Pipeline().setStages(steps)
-
-        val Array(training, test) = data.randomSplit(Array(0.70, 0.30), seed = 12345)
-
-        val model = pipeline.fit(training)
-
-        val holdout = model.transform(test).select("prediction", "DelayOutputVar")
-
-        val rm = new RegressionMetrics(holdout.rdd.map(x =>
-          (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
-
-        println("sqrt(MSE): " + Math.sqrt(rm.meanSquaredError))
-        println("mean absolute error: " + 	rm.meanAbsoluteError)
-        println("R Squared: " + rm.r2)                         
-        println("Explained Variance: " + rm.explainedVariance + "\n")
-      /*
-      case _ =>
-        val gbt = new GBTRegressor()
-          .setLabelCol("DelayOutputVar")
-          .setFeaturesCol("features")
-          .setMaxIter(10)
-
-        val steps:Array[org.apache.spark.ml.PipelineStage] = if(useCategorical){
-                                                                categoricalIndexers ++ categoricalEncoders ++ Array(assembler, gbt)
-                                                              }else{
-                                                                Array(assembler, gbt)
-                                                              }
-
-        val pipeline = new Pipeline().setStages(steps)
-
-
-        val Array(training, test) = data.randomSplit(Array(0.70, 0.30), seed = 12345)
-
-        val model = pipeline.fit(training)
-
-        val holdout = model.transform(test).select("prediction", "DelayOutputVar")
-
-        val rm = new RegressionMetrics(holdout.rdd.map(x =>
-          (x(0).asInstanceOf[Double], x(1).asInstanceOf[Double])))
-
-        println("sqrt(Mean squared error): " + Math.sqrt(rm.meanSquaredError))
-        println("Mean absolute error: " + 	rm.meanAbsoluteError)
-        println("R Squared: " + rm.r2)                         
-        println("Explained Variance: " + rm.explainedVariance + "\n")
-        */
-
-    }
     sparkCtx.stop()
   }
 
